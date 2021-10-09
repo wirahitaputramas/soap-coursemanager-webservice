3bd072be56e4a064c05d53b8d690f2c451a339d6
wirahitaputramas <wirahitaputramas@gmail.com>
2021-10-10 03:48:39 +0800

    Initial commit

---
+++ src/main/java/com/devoid/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -0,0 +1,31 @@
+package com.devoid.soap.webservices.soapcoursemanagement.soap;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.devoid.courses.CourseDetails;
+import com.devoid.courses.GetCourseDetailsRequest;
+import com.devoid.courses.GetCourseDetailsResponse;
+
+@Endpoint
+public class CourseDetailsEndpoint {
+
+	@PayloadRoot(namespace = "http://devoid.com/courses", localPart = "GetCourseDetailsRequest")
+	@ResponsePayload
+	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
+		
+		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		
+		CourseDetails courseDetails = new CourseDetails();
+		courseDetails.setId(request.getId());
+		courseDetails.setName("SOAP web services Course");
+		courseDetails.setDescription("It is a wonderful course!");
+		
+		response.setCourseDetails(courseDetails);
+		
+		return response;
+	}
+	
+}
---
+++ src/test/java/com/devoid/soap/webservices/soapcoursemanagement/SoapCourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devoid.soap.webservices.soapcoursemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
