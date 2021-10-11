f9e3bd2a3e6ddc26e7fc2cc91192b41fc6ecc5a9
wirahitaputramas <wirahitaputramas@gmail.com>
2021-10-11 18:57:34 +0800

    creating CourseDetailsService.java (before integrating to its endpoint)

---
+++ src/main/java/com/devoid/soap/webservices/soapcoursemanagement/soap/service/CourseDetailsService.java
@@ -0,0 +1,54 @@
+package com.devoid.soap.webservices.soapcoursemanagement.soap.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.devoid.soap.webservices.soapcoursemanagement.soap.bean.Course;
+
+public class CourseDetailsService {
+
+	private static List<Course> courses = new ArrayList<>();
+	
+	static {
+		Course course1 = new Course(1, "math", "math tutorial");
+		courses.add(course1);
+		
+		Course course2 = new Course(2, "social", "humanoria and economy");
+		courses.add(course2);
+		
+		Course course3 = new Course(3, "science", "basic physic and chemistry");
+		courses.add(course3);
+		
+		Course course4 = new Course(4, "tech", "basic programming");
+		courses.add(course4);	
+	}
+	
+	public Course findById(int id) {
+		for (Course course : courses) {
+			if (course.getId() == id)
+				return course;
+		}
+		return null;
+	}
+	
+	public List<Course> findAll() {
+		return courses;
+	}
+	
+	//create deleteById method
+	public int deleteById(int id) {
+		Iterator<Course> itr = courses.iterator();
+		while (itr.hasNext()) {
+			Course course = itr.next();
+			if (course.getId() == id) {
+				itr.remove();
+				return 1;
+			}
+		}
+		return 0;
+	}
+	
+	
+	
+}
